9e723352217ea42ec2c3a5bfcee2147c39c38fda
Akash Borgalli <akash.borgalli@gmail.com>
2022-04-26 09:31:54 +0530

    added main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from trainingModel import trainModel
+import json
+import os
+
+os.putenv('LANG', 'en_US.UTF-8')
+os.putenv('LC_ALL', 'en_US.UTF-8')
+
+if __name__ == "__main__":
+    try:
+        trainModelObj = trainModel()  # object initialization
+        trainModelObj.trainingModel()  # training the model for the files in the table
+    except Exception as e:
+        print("Error occured: " + str(e))
